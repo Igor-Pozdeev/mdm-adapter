983a81c365c6c9125501764c0aa32bb564a82c96
Igor <Sketch_90@mail.ru>
2025-10-21 22:45:19 +0300

    HW-19: init commit

---
+++ src/main/java/ru/igorpozdeev/mdmadapter/MdmAdapterApplication.java
@@ -0,0 +1,12 @@
+package ru.igorpozdeev.mdmadapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdmAdapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdmAdapterApplication.class, args);
+	}
+}
---
+++ src/test/java/ru/igorpozdeev/mdmadapter/MdmAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.igorpozdeev.mdmadapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdmAdapterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
